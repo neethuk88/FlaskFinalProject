00033d73d5484d9ca0c4b46dbd7905584c431a1d
neethuk88 <neetuk88@gmail.com>
2025-03-19 10:02:18 -0400

    output formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,14 @@ def emotion_detector(text_to_analyse):
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, json = input_json, headers=Headers)  # Send a POST request to the API with the text and headers
-    return response.text
+   #return response.text
+   # status_code = response.status_code
+
+    emotions = {}
+    
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions.items(), key=lambda x: x[1])
+        emotions['dominant_emotion'] = dominant_emotion[0]
+
+    return emotions
